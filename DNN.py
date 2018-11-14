36d37a812140007be171585a344a678184367dbb
Oleksiy Volkovskyi <iva7alexey@gmail.com>
2018-11-14 16:11:51 -0500

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -18,14 +18,15 @@ for i in range(589,706):
   x_labels = np.column_stack((x_labels,data.get(data.keys()[i]).values))
 
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
-
+model.add(Dense(30, input_dim=376, activation='relu'))
+model.add(Dense(30, activation='relu'))
+model.add(Dense(5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, Y, epochs=150, batch_size=10)
 
-scores = model.evaluate(X, Y)
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x_labels, anxiety_labels, epochs=150, batch_size=10)
+
+scores = model.evaluate(x_labels, anxiety_labels)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 model.save("model")
