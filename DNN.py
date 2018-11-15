f3919d59e94c29dce73a86758a5c75805e5c1f82
Oleksiy Volkovskyi <iva7alexey@gmail.com>
2018-11-15 12:28:49 -0500

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -3,19 +3,21 @@ from keras.layers import Dense
 import pandas as pd
 import numpy as np
 
+df.fillna(df.mean())
+
 data = pd.read_csv("data.csv")
 anxiety_labels = data.get("AnxietyDisorder").values
 ADHD_labels = data.get("ADHD").values
 
-x_labels = data.get(data.keys()[4]).values
-x_labels = np.column_stack((x_labels,data.get(data.keys()[7]).values))
-x_labels = np.column_stack((x_labels,data.get(data.keys()[8]).values))
+x_labels = (data.get(data.keys()[4]).values).fillna((data.get(data.keys()[4]).values).mean())
+x_labels = np.column_stack((x_labels,data.get(data.keys()[7]).values.fillna(data.get(data.keys()[7]).values.mean())))
+x_labels = np.column_stack((x_labels,data.get(data.keys()[8]).values.fillna(data.get(data.keys()[8]).values.mean())))
 
 for i in range(331,587):
-  x_labels = np.column_stack((x_labels,data.get(data.keys()[i]).values))
+  x_labels = np.column_stack((x_labels,data.get(data.keys()[i]).values.fillna(data.get(data.keys()[i]).values.mean())))
 
 for i in range(589,706):
-  x_labels = np.column_stack((x_labels,data.get(data.keys()[i]).values))
+  x_labels = np.column_stack((x_labels,data.get(data.keys()[i]).values.fillna(data.get(data.keys()[i]).values.mean())))
 
 model = Sequential()
 model.add(Dense(30, input_dim=376, activation='relu'))
