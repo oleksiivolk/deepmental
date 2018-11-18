d6162cff904e14d815f1edee2bba44a04aee2c5f
Oleksiy Volkovskyi <iva7alexey@gmail.com>
2018-11-18 11:12:21 -0500

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -31,17 +31,17 @@ model.add(Dense(30, activation='relu',
                kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
 model.add(Dense(30, activation='relu',
                kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
-model.add(Dense(30, activation='relu',
-               kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
-model.add(Dense(30, activation='relu',
-               kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
+#model.add(Dense(30, activation='relu',
+#              kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
+#model.add(Dense(30, activation='relu',
+#               kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
 model.add(Dense(1, activation='sigmoid'))
 
 
 nadam = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
 model.compile(loss='binary_crossentropy', optimizer=nadam, metrics=['accuracy'])
 
-model.fit(x_labels, anxiety_labels, epochs=1500, batch_size=20)
+model.fit(x_labels, anxiety_labels, epochs=150, batch_size=32)
 
 scores = model.evaluate(x_labels, anxiety_labels)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
