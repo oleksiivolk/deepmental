00f26e8b309b03d43d29cc0216c619d34ebc91f8
Oleksiy Volkovskyi <iva7alexey@gmail.com>
2018-11-20 09:38:41 -0500

    Update DNN2.py

--- DNN2.py
+++ DNN2.py
@@ -9,6 +9,7 @@ import numpy as np
 data = pd.read_csv("data.csv")
 anxiety_labels = data.get("AnxietyDisorder").values
 ADHD_labels = data.get("ADHD").values
+y_labels = anxiety_labels*2 + ADHD_labels
 
 x_labels = (data.get(data.keys()[4])).fillna((data.get(data.keys()[4])).mean())
 x_labels = np.column_stack((x_labels,data.get(data.keys()[7]).fillna(data.get(data.keys()[7]).mean())))
@@ -35,15 +36,15 @@ model.add(Dense(10, activation='relu',
               kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
 #model.add(Dense(30, activation='relu',
 #               kernel_initializer=TruncatedNormal(mean=0.0, stddev=0.05, seed=None)))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(4, activation='sigmoid'))
 
 
 nadam = Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004)
 model.compile(loss='binary_crossentropy', optimizer=nadam, metrics=['accuracy'])
 
-history_callback =  model.fit(x_labels, anxiety_labels, epochs=1500, batch_size=64)
+history_callback =  model.fit(x_labels, y_labels, epochs=1500, batch_size=64)
 
-scores = model.evaluate(x_labels, anxiety_labels)
+scores = model.evaluate(x_labels, y_labels)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 loss_history = history_callback.history["loss"]
